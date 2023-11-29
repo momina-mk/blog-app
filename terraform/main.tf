36385804a98c05909aaa899aa9d0ebff3255dc47
Momina <149628506+momina-mk@users.noreply.github.com>
2023-11-29 12:02:43 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,17 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      Version = "~>3.27"
      
     }
   }
+ required_version = ">=0.14.9"
 }
 
 provider "aws" {
   region  = "east-us-1"
+  version = "~>3.0"
+ 
 }
 resource "aws_ecr_repository" "blog-app" {
   name                 = "blog-app"
